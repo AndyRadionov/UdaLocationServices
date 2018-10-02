0b4e41a551c401a307746e8e39bb72c9574871f5
Andrey Radionov <andyomsk@gmail.com>
2018-10-02 14:43:58 +0300

    DetectedActivities IntentService

---
+++ app/src/main/java/io/github/andyradionov/udalocationservices/DetectedActivitiesIntentService.java
@@ -0,0 +1,38 @@
+package io.github.andyradionov.udalocationservices;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Andrey Radionov
+ */
+public class DetectedActivitiesIntentService extends IntentService {
+
+    private static final String TAG = DetectedActivitiesIntentService.class.getSimpleName();
+
+    public DetectedActivitiesIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+        Intent localIntent = new Intent(Constants.BROADCAST_ACTION);
+
+        ArrayList<DetectedActivity> detectedActivities = (ArrayList<DetectedActivity>) result.getProbableActivities();
+
+        Log.i(TAG, "activities detected");
+
+        localIntent.putExtra(Constants.ACTIVITY_EXTRA, detectedActivities);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
+    }
+}
